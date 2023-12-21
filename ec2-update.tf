360239e9c26c0ac2949ae3507897d526946c6cd0
gopal <gopal1409@gmail.com>
2023-12-21 15:52:28 +0530

    ec2 instance with data and varaibles

---
+++ ec2-update.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "ec2demo" {
+
+  ami                    = data.aws_ami.amzlinux2.id 
+  ###instance type is like hardocding the instance. we need to make our code readable standardized
+  ##to call a variable var  name of the variables
+  instance_type          = var.instance_type
+  #vpc_security_group_ids = [aws_security_group.sg-webserver.id]
+  subnet_id              = "subnet-09f995142f08515ea"
+  user_data              = file("${path.module}/app/app.sh")
+
+  associate_public_ip_address = true
+  tags = {
+    Name = "myec2-instance"
+  }
+}
+#ate one sg inside that i wantt to add multiple rule
+##the above bnlock will create the ec2 instance in aws. 
+###once create the refrence to the ec2 instance is stored in terraform.tfstate file. 
