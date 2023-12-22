4d746a548db948af154146b8c9c1eeb541ad4f6f
gopal <gopal1409@gmail.com>
2023-12-22 18:50:02 +0530

    terraform with autoscaling

--- a8.alb.tf
+++ a8.alb.tf
@@ -1,4 +1,4 @@
-/*##how to deploy an alb in aws. 
+##how to deploy an alb in aws. 
 ##first we will create the target group
 resource "aws_lb_target_group" "front-lb" {
   name     = "tf-example-lb-tg"
@@ -17,14 +17,14 @@ resource "aws_lb_target_group" "front-lb" {
   }
 }
 ####attach all the instance behind the target group
-resource "aws_lb_target_group_attachment" "attach-app-servers" {
+/*resource "aws_lb_target_group_attachment" "attach-app-servers" {
   ##this resourceneed to be itreated multiple times
   count = length(aws_instance.ec2demo)
   target_group_arn = aws_lb_target_group.front-lb.arn
   ###now this target group need to be iterated
   target_id = element(aws_instance.ec2demo.*.id,count.index)
   port = 80
-}
+}*/
 ##before we create the lb we need to create a listener
 resource "aws_lb_listener" "list-alb" {
   load_balancer_arn = aws_lb.lb-front.arn 
@@ -45,4 +45,4 @@ resource "aws_lb" "lb-front" {
   security_groups = [aws_security_group.allow_http.id]
   subnets = [for subnet in aws_subnet.private : subnet.id]
   enable_deletion_protection = false 
-}*/
+}
